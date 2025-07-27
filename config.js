050d334cd3e131778de913a25c8c8511f8221c3d
AKINDUMDx <akindumd870@gmail.com>
2025-07-27 08:36:54 +0530

    Update config.js
    Signed-off-by: AKINDUMDx <akindumd870@gmail.com>

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "AKINDU-MD=zEFF0QLa#wcuIGI1LpMdnpM6NOaesvhyqD6LmslTwPFG31HZVWQc",
+SESSION_ID: process.env.SESSION_ID || "AKINDU-MD=bdcXTKqS#aehJhW-IckqPvdexVoaTA2nhMmTowZL77vh2DEW7Jug",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
